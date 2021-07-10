5ca1ba13c6ab1bc5786b9dbac17752655c650a8a
StuntsKing <Anderson.avila@sistemafiep.org.br>
2021-07-10 11:42:07 -0300

    Criação do Projeto

---
+++ APCSA3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APCSA3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
